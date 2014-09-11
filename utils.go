17ab894d9b01df95cfc509e5db222ab1d9a5674f
elvuel <elvuel@gmail.com>
2014-09-11 14:36:18 +0800

    add Utils#Round

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package aqi
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
@@ -9,4 +10,20 @@ func TruncateFloat(v float64, digit int) float64 {
 	i := fmt.Sprintf("%"+fmt.Sprintf(".%df", digit), v)
 	f, _ := strconv.ParseFloat(i, digit)
 	return f
+}
+
+// see: https://github.com/DeyV/gotools
+func Round(x float64, prec int) float64 {
+	var rounder float64
+	pow := math.Pow(10, float64(prec))
+	intermed := x * pow
+
+	if intermed < 0.0 {
+		intermed -= 0.5
+	} else {
+		intermed += 0.5
+	}
+	rounder = float64(int64(intermed))
+
+	return rounder / float64(pow)
 }
