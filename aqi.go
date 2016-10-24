4e34f1a7b794f0954b0d7cc89e2ad8dc0006d6b5
elvuel <elvuel@gmail.com>
2016-10-24 10:12:57 +0800

    remove initialize information

--- aqi.go
+++ aqi.go
@@ -1,10 +1,6 @@
 // Package aqi calculate AQI
 package aqi
 
-import (
-	"fmt"
-)
-
 const (
 	Author  = "elvuel"
 	Email   = "elvuel@gmail.com"
@@ -12,12 +8,4 @@ const (
 	Licence = "MIT"
 )
 
-func init() {
-	fmt.Printf(
-		"Author: %s\nEmail: %s\nVersion: %s\nLicence: %s\n(Initialize information will be removed)\n\n",
-		Author,
-		Email,
-		Version,
-		Licence,
-	)
-}
+func init() {}
